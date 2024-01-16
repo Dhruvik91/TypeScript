e63ca615ddb950533b260068458925547aa94443
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2024-01-16 14:42:27 +0530

    Learn funciton declaration

---
+++ functions/function.js
@@ -0,0 +1,12 @@
+// here we will learn about the function decleration 
+function hello() {
+    return ("hello");
+}
+function hi() {
+    return "hi";
+}
+function bye() {
+}
+console.log(hello());
+console.log(hi());
+console.log(bye());
---
+++ functions/function.ts
@@ -0,0 +1,17 @@
+// here we will learn about the function decleration 
+
+
+function hello() {                // this is a simple function 
+    return ("hello");
+}
+
+function hi(): string {
+    return "hi"
+}
+
+function bye(): void {
+
+}
+console.log(hello())
+console.log(hi())
+console.log(bye())
