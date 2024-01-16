fd7f4de849d113b6d48d7729a08a4ea7b778588d
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2024-01-16 14:25:50 +0530

    Fisrt commit

---
+++ array/array.ts
@@ -0,0 +1,15 @@
+// undeclared array
+let array = ["ravi", "sahil", "rishi"]
+array.push("5")       // here we can only push same type value
+
+console.log(array)
+
+let array2 = ["!", 1, "2"]; // this array is called a mix array, where we can add both type vales
+
+array2.push(3);
+array2.push("6");
+
+console.log("The second array", array2)
+
+
+const arr : string[] = ["ravi"]; // this is pre defined array, where we can just add the specific values
